8130b5cd728ae96612d260cb6dd29125348c7578
Will Hall <willhall88@hotmail.com>
2014-04-20 12:57:14 +0100

    added a new test and added more to some of the previous tests

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,10 +1,25 @@
 class Airport
+  DEFAULT_CAPACITY = 6
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+    @planes = []
+  end
+
+  def plane_count
+    @planes.count
+  end
 
   def clear_to_takeoff(plane)
     plane.takeoff
+    @planes.delete(plane)
   end
 
   def clear_to_land(plane)
     plane.land
+    @planes << plane
+  end
+
+  def full?
+    @planes.count == @capacity
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,24 +2,32 @@ require "airport"
 
 describe Airport do
   
-  it "a plane can takeoff" do
-
-    airport = Airport.new
-    plane = double :plane
-
-    expect(plane).to receive(:takeoff)
-
-    airport.clear_to_takeoff(plane)
-
-  end
+  let(:airport) {Airport.new}
+  let(:plane) {double :plane}
 
   it "a plane can land" do
-    airport = Airport.new
-    plane = double :plane
-
+    expect(airport.plane_count).to eq 0
     expect(plane).to receive(:land)
 
     airport.clear_to_land(plane)
+    expect(airport.plane_count).to eq 1
+  end
+
+  it "a plane can takeoff" do
+    expect(plane).to receive(:takeoff)
+
+    airport.clear_to_takeoff(plane)
+    expect(airport.plane_count).to eq 0
+  end
+
+  it "can be full" do
+    plane = double :plane, land: :plane
+    expect(airport).not_to be_full
+
+    6.times {airport.clear_to_land(plane)}
+
+    expect(airport).to be_full
+
   end
 
 end
