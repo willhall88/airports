908b82b4c205a7639d4187813688a5412049d42e
Will Hall <willhall88@hotmail.com>
2014-04-20 13:38:53 +0100

    added some context to the airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,35 +5,41 @@ describe Airport do
   let(:airport) {Airport.new}
   let(:plane) {double :plane, land: :plane}
 
-  it "a plane can land" do
-    expect(airport.plane_count).to eq 0
-    expect(plane).to receive(:land)
+  context "take off and landing" do
+    it "a plane can land" do
+      expect(airport.plane_count).to eq 0
+      expect(plane).to receive(:land)
 
-    airport.clear_to_land(plane)
-    expect(airport.plane_count).to eq 1
-  end
+      airport.clear_to_land(plane)
+      expect(airport.plane_count).to eq 1
+    end
 
-  it "a plane can takeoff" do
-    expect(plane).to receive(:takeoff)
+    it "a plane can takeoff" do
+      expect(plane).to receive(:takeoff)
 
-    airport.clear_to_takeoff(plane)
-    expect(airport.plane_count).to eq 0
-  end
-
-  it "can be full" do
-
-    expect(airport).not_to be_full
-
-    6.times {airport.clear_to_land(plane)}
-
-    expect(airport).to be_full
+      airport.clear_to_takeoff(plane)
+      expect(airport.plane_count).to eq 0
+    end
 
   end
 
-  it "a plane cannot land if the airport is full" do
-    6.times {airport.clear_to_land(plane)}
+  context "traffic control" do
+    it "can be full" do
 
-    expect{airport.clear_to_land(plane)}.to raise_error(RuntimeError)
+      expect(airport).not_to be_full
+
+      6.times {airport.clear_to_land(plane)}
+
+      expect(airport).to be_full
+
+    end
+
+    it "a plane cannot land if the airport is full" do
+      6.times {airport.clear_to_land(plane)}
+
+      expect{airport.clear_to_land(plane)}.to raise_error(RuntimeError)
+
+    end
 
   end
 
